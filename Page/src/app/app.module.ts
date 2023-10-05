dbaf5bc9faa37f832f03bd6fd811f4300d410d1a
katip69 <miguelrasero1999@hotmail.es>
2023-10-05 18:56:37 +0200

    Pagina de Inicio

--- Page/src/app/app.module.ts
+++ Page/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
