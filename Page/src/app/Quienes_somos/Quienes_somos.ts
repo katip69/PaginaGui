a48eac3ad0bf2ff016163bc55107d2d5f5b6c187
katip69 <miguelrasero1999@hotmail.es>
2023-10-27 21:27:29 +0200

    nav y quienes somos inicio

---
+++ Page/src/app/Quienes_somos/Quienes_somos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Quienes-somos',
+  templateUrl: './Quienes_somos.html',
+  styleUrls: ['./Quienes_somos.css']
+})
+export class QuienesSomos {
+
+}
