95ae396043df4fa51267d30a6eb3cfeac1cd05ff
katip69 <miguelrasero1999@hotmail.es>
2023-11-22 12:50:44 +0100

    Finalizacion paginas estaticas

--- Page/src/app/app.module.ts
+++ Page/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { QuienesSomos } from './Quienes_somos/Quienes_somos';
 import { RouterModule, Routes } from '@angular/router';
 import { Inicio } from './Inicio/Inicio';
 import { Contacto } from './Contacto/Contacto';
+import { Actividades } from './Actividades/Actividades';
 
 
 const appRoutes:Routes=[
 
   {path:'',component:Inicio},
   {path:'QuienesSomos', component:QuienesSomos},
-  {path:'Contacto', component:Contacto}
+  {path:'Contacto', component:Contacto},
+  {path:'Actividades', component:Actividades}
   
 ];
 
